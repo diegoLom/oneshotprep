329324c17695518b1b32ebc9d717c039b5692616
diegoLom <diegosantana019@gmail.com>
2025-04-07 17:11:25 -0300

    Update postscript.sh

--- userpreference/postscript.sh
+++ userpreference/postscript.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo cp /home/ec2-user/deploy/userpreference.service /etc/systemd/system/  # Ensure the file exists
+sudo chown root:root /etc/systemd/system/userpreference.service  # Correct ownership
 sudo systemctl daemon-reload
 sudo systemctl enable userpreference.service
 sudo systemctl start userpreference.service
